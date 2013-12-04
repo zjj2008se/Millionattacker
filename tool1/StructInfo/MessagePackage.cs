ca9516bf407591329eafc5e2ae7073d938be06cd
zjj2008se <hero2008se@gmail.com>
2013-12-04 10:13:00 +0800

    添加json库

---
+++ tool1/StructInfo/MessagePackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MillionTools.tool1.StructInfo
+{
+    class MessagePackage
+    {
+        public string message;
+        public Object result;
+
+    }
+}
